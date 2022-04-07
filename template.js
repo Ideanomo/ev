7b5bd2f1260c9e363d089fcfebe86feacda0a678
Daniel <ds23@hotmail.co.uk>
2022-04-07 18:37:20 +0100

    Create template

---
+++ template.js
@@ -0,0 +1,12 @@
+export default () => {
+    return `<!doctype HTML>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <title>EV Fullstack app</title>
+            </head>
+            <body>
+                <div id="root">Hello World</div>
+            </body>
+        </html>`
+}
