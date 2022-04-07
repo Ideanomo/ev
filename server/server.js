17c5c57e72afb6dad1aa9f3e6a886fc4ea7fee8f
Daniel <ds23@hotmail.co.uk>
2022-04-07 18:27:33 +0100

    Connect to MongoDB and set up Mongoose

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 import config from './../config/config';
 import app from './express';
+import mongoose from 'mongoose';
 
 const server = app.listen(config.port, (err) => {
     if (err) {
         console.log(err)
     }
     console.log(`Server started on port ${config.port}`)
+});
+
+// Allow Mongoose to use ES6 Promises
+mongoose.Promise = global.Promise;
+mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+mongoose.connection.on('error', () => {
+    throw new Error(`Unable to connect to database: ${config.mongoUri}`)
 })
+
