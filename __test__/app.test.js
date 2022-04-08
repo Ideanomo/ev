6cec3ebf37657a0c8fbfc3b6185931d0bc601174
Daniel <ds23@hotmail.co.uk>
2022-04-08 16:52:19 +0100

    Update test

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -1,34 +1,46 @@
-const supertest = require('supertest');
-const app = require('./../server/express');
-const http = require('http');
-let request;
+let request = require('supertest');
+const app = require("./../server/express");
+const http = require("http");
 let server = http.createServer(app);
 
-describe('GET api/clients', () => {
+describe('/api/clients', () => {
     beforeAll(async () => {
-        request = await supertest(server)
+        request = await request(server)
     })
 
-    test('It should return 200', async () => {
-        request.get('/api/clients')
-            .expect(200)
-    })
-
-    test('It should specify json as content type in http header', async () => {
-        request.post('/api/clients').send({
-            name: "name",
-            email: "name@name.info",
-            company: "company",
-            password: "password"
+    describe('GET /', () => {
+        test('It should return 200', async () => {
+            request.get('/api/clients')
+                .expect(200)
         })
-            .expect('Content-Type', /json/)
-            .expect(200)
     })
 
-    test('It should return 400 if all fields are not entered at signup', () => {
-        request.post('/api/clients').send({
-            name: "username"
+    describe('GET /api/clients', () => {
+        test('It should return all clients', async () => {
+            request.get('/api/clients')
+                .expect(200)
         })
-            .expect(400)
+    })
+
+    describe('POST /api/clients', () => {
+        test('It should specify json as content type in http header ', () => {
+            request.post('/api/clients').send({
+                name: "name",
+                email: "name@name.info",
+                company: "company",
+                password: "password"
+            })
+                .expect('Content-Type', /json/)
+                .expect(200)
+        })
+    })
+
+    describe('POST /api/clients', () => {
+        test('It should return 400 if name email and password not entered at signup', () => {
+            request.post('/api/clients').send({
+                name: "name",
+            })
+                .expect(400)
+        })
     })
 })
