fc19b19ed055bd4526b667f6f9655353be08ca08
Daniel <ds23@hotmail.co.uk>
2022-04-08 01:21:00 +0100

    Implement client model

---
+++ server/models/client.model.js
@@ -0,0 +1,77 @@
+import mongoose from 'mongoose';
+import crypto from 'crypto';
+import dayjs from 'dayjs';
+
+const ClientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: 'Name is required'
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 'Email already exist',
+        match: [/.+@.+\..+/, 'Please fill a valid email address'],
+        required: 'Email is required'
+    },
+    company: {
+        type: String,
+        trim: true,
+        required: 'Company is required'
+    },
+    createdDate: {
+        type: String,
+        default: dayjs().format('DD/MM/YYYY')
+    },
+    hashed_password: {
+        type: String,
+        required: 'Please enter a valid password'
+    },
+    salt: String
+})
+
+ClientSchema
+    .virtual('password')
+    .set(function(password) {
+        this._password = password
+        this.salt = this.makeSalt()
+        this.hashed_password = this.encryptPassword(password)
+    })
+    .get(function () {
+        return this._password
+    })
+
+ClientSchema.methods = {
+    authenticate: function (plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password
+    },
+    encryptPassword: function (password) {
+        if (!password) {
+            return '';
+        }
+        try {
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex')
+        } catch (err) {
+            return '';
+        }
+    },
+    makeSalt: function () {
+        return Math.round((new Date().valueOf() * Math.random())) + ''
+    }
+}
+
+ClientSchema.path('hashed_password').validate(function() {
+    if (this._password && this._password.length < 6) {
+        this.invalidate('password', 'Password must be at least 6 characters.')
+    }
+    if (this.isNew && !this._password) {
+        this.invalidate('password', 'Password is required')
+    }
+}, null);
+
+
+export default mongoose.model('Client', ClientSchema);
